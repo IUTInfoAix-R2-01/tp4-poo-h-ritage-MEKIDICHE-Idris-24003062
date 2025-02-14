3add67aa2ff277c51dd1bc662e86707323a6a8ef
Mekidiche <MEKIDICHE-Idris-24003062>
2025-02-14 12:15:57 +0100

    commit de fin de séance

--- src/tp3exosYaip4/Student.java
+++ src/tp3exosYaip4/Student.java
@@ -12,4 +12,26 @@ public class Student extends Person{
 		this.year = year;
 		this.fee = fee;
 	}
+	public String getProgram() {
+		return program;
+	}
+	public void setProgram(String program) {
+		this.program = program;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public double getFee() {
+		return fee;
+	}
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+	public String toString() {
+		return "Strudent[Person[name = " + name + ",address" + address + ",program" + program + ",year" + year + "fee" + fee "]";
+
+	}
 }
